85aa08aabbe6069533f97a0fcec73a96599c6810
danimisme <msubhanr53@gmail.com>
2024-03-21 23:27:44 +0700

    Optional Catch-all Segments

--- src/app/product/[[...slug]]/page.tsx
+++ src/app/product/[[...slug]]/page.tsx
@@ -10,9 +10,13 @@ export default function DetailProductPage(props: DetailProductPageProps) {
   return (
     <div>
       <h1>Detail Product Page </h1>
-      <p>Category : {params.slug[0]}</p>
-      <p>Gender : {params.slug[1]}</p>
-      <p>Product : {params.slug[2]}</p>
+      {params.slug && (
+        <>
+          <p>Category : {params.slug[0]}</p>
+          <p>Gender : {params.slug[1]}</p>
+          <p>Product : {params.slug[2]}</p>
+        </>
+      )}
     </div>
   );
 }
