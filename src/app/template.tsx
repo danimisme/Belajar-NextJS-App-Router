27130b3f20c4e33ed320e3eabb7a95b6742b9bb9
danimisme <msubhanr53@gmail.com>
2024-03-21 22:54:44 +0700

    Layout and Template

---
+++ src/app/template.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { useState } from "react";
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1 className="text-blue-800">Template {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Click template</button>
+      {children}
+    </div>
+  );
+}
