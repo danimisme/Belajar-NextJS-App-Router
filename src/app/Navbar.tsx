f61417e0afdacac841a72385b6edbc6d99b9bd7c
danimisme <msubhanr53@gmail.com>
2024-03-22 21:53:33 +0700

    learn how to usePathname to check pathname in navbar navigation

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,19 +1,39 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
   return (
     <div>
       <nav className="flex bg-gray-800 py-2 px-5">
         <h1 className="text-white">Navbar</h1>
         <ul className="flex ml-auto">
           <Link href="/">
-            <li className="mr-3 text-blue-300 cursor-pointer">Home</li>
+            <li
+              className={`mr-3 ${
+                pathname === "/" ? "text-blue-300" : "text-white"
+              } cursor-pointer`}
+            >
+              Home
+            </li>
           </Link>
           <Link href="/about">
-            <li className="mr-3 text-blue-300 cursor-pointer">About</li>
+            <li
+              className={`mr-3 ${
+                pathname === "/about" ? "text-blue-300" : "text-white"
+              } cursor-pointer`}
+            >
+              About
+            </li>
           </Link>
           <Link href="/about/profile">
-            <li className="mr-3 text-blue-300 cursor-pointer">Profile</li>
+            <li
+              className={`mr-3 ${
+                pathname === "/about/profile" ? "text-blue-300" : "text-white"
+              } cursor-pointer`}
+            >
+              Profile
+            </li>
           </Link>
         </ul>
       </nav>
