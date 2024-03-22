67ce7708fbdd03f9da140905053923cf0fe87171
danimisme <msubhanr53@gmail.com>
2024-03-22 22:05:52 +0700

    learn how to navigate using useRoute

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Navbar() {
   const pathname = usePathname();
+  const router = useRouter();
   return (
-    <div>
-      <nav className="flex bg-gray-800 py-2 px-5">
+    <nav className="flex bg-gray-800 py-2 px-5 justify-between">
+      <div className="flex">
         <h1 className="text-white">Navbar</h1>
-        <ul className="flex ml-auto">
+        <ul className="flex ml-5">
           <Link href="/">
             <li
               className={`mr-3 ${
@@ -36,7 +37,15 @@ export default function Navbar() {
             </li>
           </Link>
         </ul>
-      </nav>
-    </div>
+      </div>
+      <div>
+        <button
+          className="bg-white rounded-md px-3 text-sm h-7 cursor-pointer"
+          onClick={() => router.push("/login")}
+        >
+          Login
+        </button>
+      </div>
+    </nav>
   );
 }
